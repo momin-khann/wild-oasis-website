0e88672859bc64dedbfea8d78f7b19519c4842fe
Momin Khan <mominkhan.dev@gmail.com>
2024-06-05 17:17:35 +0500

    feat: supabase, pages and code structure

---
+++ app/loading.tsx
@@ -0,0 +1,5 @@
+import Spinner from "@/components/reusable/Spinner";
+
+export default function Loading() {
+  return <Spinner />;
+}
---
+++ components/CabinList.tsx
@@ -0,0 +1,20 @@
+import { getCabins } from "@/services";
+import CabinCard from "@/components/reusable/CabinCard";
+
+async function CabinList() {
+  // noStore();
+
+  const cabins = await getCabins();
+
+  if (!cabins.length) return null;
+
+  return (
+    <div className="grid sm:grid-cols-1 md:grid-cols-2 gap-8 lg:gap-12 xl:gap-14 pt-6 pb-16">
+      {cabins.map((cabin) => (
+        <CabinCard cabin={cabin} key={cabin.id} />
+      ))}
+    </div>
+  );
+}
+
+export default CabinList;
